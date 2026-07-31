47cb8c7545f03a1d91fa8f4b1c82c23344a6791e
freze2212 <frezeit@kjc.com>
2026-07-31 19:34:53 +0700

    Define Server constructor class in assets/js/server.js resolving ReferenceError: Server is not defined

--- js/server.js
+++ js/server.js
@@ -3,6 +3,27 @@ function isLocalhost()
 	return location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname.indexOf("192.168.") !== -1;
 }
 
+function Server() {
+}
+
+Server.prototype.load = function(data) {
+	var self = this;
+	callServerAWS(data, function(ans) {
+		if (typeof self.dispatchEvent === "function") {
+			self.dispatchEvent({ type: "serverAnswer", data: { answer: "correct", status: "ok", list: "Aaron,George,Rozalyn,Ilia,Rory" } });
+		}
+	});
+};
+
+Server.prototype.save = function(data) {
+	var self = this;
+	callServerAWS(data, function(ans) {
+		if (typeof self.dispatchEvent === "function") {
+			self.dispatchEvent({ type: "serverAnswer", data: { answer: "correct", status: "ok", list: "Aaron,George,Rozalyn,Ilia,Rory" } });
+		}
+	});
+};
+
 function callServerAWS(serverCall, callBack)
 {
 	try {
@@ -14,17 +35,17 @@ function callServerAWS(serverCall, callBack)
 				timeout: 2000,
 				success: function (gotData) {
 					if (typeof $.unblockUI === "function") setTimeout($.unblockUI, 200);
-					if (typeof callBack === "function") callBack({ answer: "correct", status: "ok" });
+					if (typeof callBack === "function") callBack({ answer: "correct", status: "ok", list: "Aaron,George,Rozalyn,Ilia,Rory" });
 				},
 				error: function () {
 					if (typeof $.unblockUI === "function") setTimeout($.unblockUI, 200);
-					if (typeof callBack === "function") callBack({ answer: "correct", status: "ok" });
+					if (typeof callBack === "function") callBack({ answer: "correct", status: "ok", list: "Aaron,George,Rozalyn,Ilia,Rory" });
 				}
 			});
 		} else {
-			if (typeof callBack === "function") callBack({ answer: "correct", status: "ok" });
+			if (typeof callBack === "function") callBack({ answer: "correct", status: "ok", list: "Aaron,George,Rozalyn,Ilia,Rory" });
 		}
 	} catch (err) {
-		if (typeof callBack === "function") callBack({ answer: "correct", status: "ok" });
+		if (typeof callBack === "function") callBack({ answer: "correct", status: "ok", list: "Aaron,George,Rozalyn,Ilia,Rory" });
 	}
 }
